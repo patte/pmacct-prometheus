6da1bc84d609c086d0cbca461a09219ccac98caf
patte <p@tte.io>
2021-09-24 05:50:49 +0200

    geocoding (country,asn) for flows

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/patte/go-pmacct
 go 1.16
 
 require (
-	github.com/oschwald/geoip2-golang v1.5.0 // indirect
-	github.com/prometheus/client_golang v1.11.0 // indirect
+	github.com/oschwald/geoip2-golang v1.5.0
+	github.com/prometheus/client_golang v1.11.0
+	inet.af/netaddr v0.0.0-20210903134321-85fa6c94624e // indirect
+	tailscale.com v1.14.3
 )
--- pipe.go
+++ pipe.go
@@ -3,11 +3,9 @@ package main
 import (
 	"bufio"
 	"encoding/json"
-	"errors"
 	"flag"
 	"fmt"
 	"log"
-	"net"
 	"net/http"
 	"os"
 	"os/exec"
@@ -20,82 +18,193 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"inet.af/netaddr"
 
 	"github.com/oschwald/geoip2-golang"
-)
 
-// {"event_type": "purge", "ip_src": "10.0.1.1", "ip_dst": "10.0.2.1", "packets": 2, "bytes": 143}
-type Peer struct {
-	Ip      net.IP
-	Country string
-	City    string
-	Asn     string
-}
-type flow struct {
-	IpSrc    string `json:"ip_src"`
-	IpDst    string `json:"ip_dst"`
-	Packages int    `json:"packets"`
-	Bytes    int    `json:"bytes"`
-}
+	"tailscale.com/net/interfaces"
+)
 
 var (
 	addr    = flag.String("addr", ":9590", "Listening Address for /metrics")
 	verbose = flag.Bool("verbose", false, "Be chatty on stdout")
 )
 
-var (
-	flowBytesTotal = promauto.NewCounterVec(
-		prometheus.CounterOpts{
-			Name: "flow_bytes_total",
-			Help: "Flow Bytes.",
-		},
-		[]string{"ip_src", "ip_dst", "country_src", "country_dst", "asn_src", "asn_dst"},
-	)
-	flowPackagesTotal = promauto.NewCounterVec(
-		prometheus.CounterOpts{
-			Name: "flow_packages_total",
-			Help: "Flow Packages.",
-		},
-		[]string{"ip_src", "ip_dst"},
-	)
-)
+// {"event_type": "purge", "ip_src": "10.0.1.1", "ip_dst": "10.0.2.1", "packets": 2, "bytes": 143}
+type Flow struct {
+	IpSrcRaw    string `json:"ip_src"`
+	IpDstRaw    string `json:"ip_dst"`
+	IpSrc       netaddr.IP
+	IpDst       netaddr.IP
+	Packages    int    `json:"packets"`
+	Bytes       int    `json:"bytes"`
+	Proto       string `json:"proto"`
+	Direction   string
+	Private     bool
+	PrivateRaw  string
+	Source      *Peer
+	Destination *Peer
+}
+type Peer struct {
+	Ip         netaddr.IP
+	Country    string
+	CountryISO string
+	City       string
+	Asn        string
+	AsnOrg     string
+	Latitude   float64
+	Longitude  float64
+}
+
+func MakeFlow(text string, localIps []netaddr.IP, dbCity *geoip2.Reader, dbASN *geoip2.Reader) (*Flow, error) {
+	f := Flow{}
+	if err := json.Unmarshal([]byte(text), &f); err != nil {
+		return nil, err
+	}
+
+	source, err := MakePeer(f.IpSrcRaw, dbCity, dbASN)
+	if err != nil && *verbose {
+		return nil, err
+	}
+	destination, err := MakePeer(f.IpDstRaw, dbCity, dbASN)
+	if err != nil && *verbose {
+		return nil, err
+	}
+
+	f.IpSrc = source.Ip
+	f.IpDst = destination.Ip
+
+	f.Source = source
+	f.Destination = destination
+
+	f.Direction = GetDirection(f, localIps)
+	f.Private = source.Ip.IsPrivate() && destination.Ip.IsPrivate()
+	if f.Private {
+		f.PrivateRaw = "private"
+	} else {
+		f.PrivateRaw = "public"
+	}
+
+	return &f, nil
+}
 
 func MakePeer(ipRaw string, dbCity *geoip2.Reader, dbASN *geoip2.Reader) (*Peer, error) {
-	ip := net.ParseIP(ipRaw)
-	if ip == nil {
-		return nil, errors.New("ip invalid")
+	ip, err := netaddr.ParseIP(ipRaw)
+	if err != nil {
+		return nil, err
 	}
-	// if len(record.Subdivisions) > 0 {
-	// 	fmt.Printf("subdivision name: %v\n", record.Subdivisions[0].Names["en"])
-	// }
-	// fmt.Printf("ISO country code: %v\n", record.Country.IsoCode)
-	// fmt.Printf("Time zone: %v\n", record.Location.TimeZone)
-	// fmt.Printf("Coordinates: %v, %v\n", record.Location.Latitude, record.Location.Longitude)
 
 	var country string
+	var countryISO string
 	var city string
-	cityRecord, _ := dbCity.City(ip)
+	var latitude float64
+	var longitude float64
+	cityRecord, _ := dbCity.City(ip.IPAddr().IP)
 	if cityRecord != nil {
 		country = cityRecord.Country.Names["en"]
+		countryISO = cityRecord.Country.IsoCode
 		city = cityRecord.City.Names["en"]
+		latitude = cityRecord.Location.Latitude
+		longitude = cityRecord.Location.Longitude
 	}
 
 	var asn string
-	asnRecord, _ := dbASN.ASN(ip)
+	var asnOrg string
+	asnRecord, _ := dbASN.ASN(ip.IPAddr().IP)
 	if asnRecord != nil {
 		asn = strconv.FormatUint(uint64(asnRecord.AutonomousSystemNumber), 10)
+		asnOrg = asnRecord.AutonomousSystemOrganization
 	}
 
 	return &Peer{
-		Ip:      ip,
-		Country: country,
-		City:    city,
-		Asn:     asn,
+		Ip:         ip,
+		Country:    country,
+		CountryISO: countryISO,
+		City:       city,
+		Asn:        asn,
+		AsnOrg:     asnOrg,
+		Latitude:   latitude,
+		Longitude:  longitude,
 	}, nil
+}
+
+func containsIP(ips []netaddr.IP, ip netaddr.IP) bool {
+	for _, ip1 := range ips {
+		if ip1 == ip {
+			return true
+		}
+	}
+	return false
+}
+
+func GetDirection(f Flow, localIps []netaddr.IP) string {
+	if containsIP(localIps, f.IpDst) {
+		return "in"
+	}
+	if containsIP(localIps, f.IpSrc) {
+		return "out"
+	}
+	return "unknown"
+}
+
+var (
+	flowDirectionBytes = promauto.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "flow_direction_bytes",
+			Help: "in or out Bytes",
+		},
+		[]string{"direction", "private", "country", "asn", "asn_org"},
+	)
+	// flowBytesTotal = promauto.NewCounterVec(
+	// 	prometheus.CounterOpts{
+	// 		Name: "flow_bytes_total",
+	// 		Help: "Flow Bytes.",
+	// 	},
+	// 	[]string{"ip_src", "ip_dst", "country_src", "country_dst", "asn_src", "asn_dst", "direction"},
+	// )
+)
+
+func LogPrometheus(flow *Flow) {
+	if flow.Direction == "in" || flow.Direction == "out" {
+		var peer *Peer
+		if flow.Direction == "in" {
+			peer = flow.Source
+		} else {
+			peer = flow.Destination
+		}
+		flowDirectionBytes.With(
+			prometheus.Labels{
+				"direction": flow.Direction,
+				"private":   flow.PrivateRaw,
+				// "ip":        peer.Ip.String(),
+				"country": peer.Country,
+				"asn":     peer.Asn,
+				"asn_org": peer.AsnOrg,
+			},
+		).Add(float64(flow.Bytes))
+	}
+	// flowBytesTotal.With(
+	// 	prometheus.Labels{
+	// 		"ip_src":      flow.IpSrc.String(),
+	// 		"ip_dst":      flow.IpDst.String(),
+	// 		"country_src": flow.Source.Country,
+	// 		"country_dst": flow.Destination.Country,
+	// 		"asn_src":     flow.Source.Asn,
+	// 		"asn_dst":     flow.Destination.Asn,
+	// 		"direction":   flow.Direction,
+	// 	},
+	// ).Add(float64(flow.Bytes))
 }
 
 func main() {
 	flag.Parse()
+
+	// get local ip addresses
+	localIps, _, err := interfaces.LocalAddresses()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Local ips: %s\n", localIps)
 
 	// open geo databases
 	dbCity, err := geoip2.Open("GeoLite2-City.mmdb")
@@ -117,16 +226,6 @@ func main() {
 		http.ListenAndServe(*addr, nil)
 	}()
 
-	// exec command: pmacctd
-	cmd := exec.Command("pmacctd", "-r 1", "-c src_host,dst_host", "-P print", "-O json")
-	stdout, err := cmd.StdoutPipe()
-	if err != nil {
-		log.Fatal(err)
-	}
-	if err := cmd.Start(); err != nil {
-		log.Fatalf("cmd.Start() failed with '%s'\n", err)
-	}
-
 	// wait for either a term signal or a message indicating shutdown
 	var wg sync.WaitGroup
 	wg.Add(1)
@@ -140,50 +239,41 @@ func main() {
 		wg.Done()
 	}()
 
+	// exec command: pmacctd
+	// https://github.com/pmacct/pmacct/blob/master/QUICKSTART
+	// https://github.com/pmacct/pmacct/blob/6579ebeccdd0dd33e013a20a0b12a89c1bd65e94/sql/pmacct-create-table_v9.pgsql
+	//
+	cmd := exec.Command("pmacctd", "-r 1", "-c src_host,dst_host,src_port,dst_port,proto", "-P print", "-O json")
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := cmd.Start(); err != nil {
+		log.Fatalf("cmd.Start() failed with '%s'\n", err)
+	}
 	// handle stdout of pmacctd
 	scanner := bufio.NewScanner(stdout)
 	go func() {
 		for scanner.Scan() {
 			text := scanner.Text()
 			if strings.HasPrefix(text, "{") {
-				var f flow
-				if err := json.Unmarshal([]byte(text), &f); err != nil {
+
+				flow, err := MakeFlow(text, localIps, dbCity, dbASN)
+				if err != nil {
 					log.Fatal(err)
 				}
 
-				source, err := MakePeer(f.IpSrc, dbCity, dbASN)
-				if err != nil && *verbose {
-					log.Print(err)
-				}
-				destination, err := MakePeer(f.IpDst, dbCity, dbASN)
-				if err != nil && *verbose {
-					log.Print(err)
-				}
-
 				if *verbose {
-					fmt.Printf("%+v\n%+v\n%+v\n\n", f, source, destination)
+					// fmt.Printf("%s\n", text)
+					fmt.Printf("%+v\n%+v\n%+v\n\n", flow, flow.Source, flow.Destination)
 				}
 
-				flowBytesTotal.With(
-					prometheus.Labels{
-						"ip_src":      f.IpSrc,
-						"ip_dst":      f.IpDst,
-						"country_src": source.Country,
-						"country_dst": destination.Country,
-						"asn_src":     source.Asn,
-						"asn_dst":     destination.Asn,
-					},
-				).Add(float64(f.Bytes))
-				flowPackagesTotal.With(
-					prometheus.Labels{
-						"ip_src": f.IpSrc,
-						"ip_dst": f.IpDst,
-					},
-				).Add(float64(f.Packages))
+				LogPrometheus(flow)
 			} else {
 				fmt.Println(text)
+				// TODO identify exit message by pmacct
+				// wg.Done()
 			}
-			// wg.Done()
 		}
 	}()
 
